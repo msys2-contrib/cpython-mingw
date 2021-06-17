31b7b9fb4d58588f36a46946b3012789f71b0e38
Алексей <alexey.pawlow@gmail.com>
2021-06-17 18:52:07 +0530

    getcompiler: expose MINGW toolchain related information
    * include MINGW and UCRT
    * include things like "64 bit (AMD64)" for compat with the MSVC build

--- Python/getcompiler.c
+++ Python/getcompiler.c
@@ -7,10 +7,49 @@
 
 // Note the __clang__ conditional has to come before the __GNUC__ one because
 // clang pretends to be GCC.
-#if defined(__clang__)
+#if defined(__clang__) && !defined(_WIN32)
 #define COMPILER "[Clang " __clang_version__ "]"
 #elif defined(__GNUC__)
-#define COMPILER "[GCC " __VERSION__ "]"
+/* To not break compatibility with things that determine
+   CPU arch by calling get_build_version in msvccompiler.py
+   (such as NumPy) add "32 bit" or "64 bit (AMD64)" on Windows
+   and also use a space as a separator rather than a newline. */
+#if defined(_WIN32)
+#  define COMP_SEP " "
+#  if defined(__x86_64__)
+#    define ARCH_SUFFIX " 64 bit (AMD64)"
+#  elif defined(__aarch64__)
+#    define ARCH_SUFFIX " 64 bit (ARM64)"
+#  elif defined(__arm__)
+#    define ARCH_SUFFIX " 32 bit (ARM)"
+#  else
+#    define ARCH_SUFFIX " 32 bit"
+#  endif
+#else
+#  define COMP_SEP "\n"
+#  define ARCH_SUFFIX ""
+#endif
+
+#if defined __MINGW32__
+#  define ARCH_PREFIX "MINGW "
+#else
+#  define ARCH_PREFIX ""
+#endif
+
+#if defined(__clang__)
+#  if defined(_UCRT)
+#    define COMPILER COMP_SEP "[" ARCH_PREFIX "Clang UCRT " __clang_version__ ARCH_SUFFIX "]"
+#  else
+#    define COMPILER COMP_SEP "[" ARCH_PREFIX "Clang " __clang_version__ ARCH_SUFFIX "]"
+#  endif
+#else
+#  if defined(_UCRT)
+#    define COMPILER COMP_SEP "[" ARCH_PREFIX "GCC UCRT " __VERSION__ ARCH_SUFFIX "]"
+#  else
+#    define COMPILER COMP_SEP "[" ARCH_PREFIX "GCC " __VERSION__ ARCH_SUFFIX "]"
+#  endif
+#endif
+
 // Generic fallbacks.
 #elif defined(__cplusplus)
 #define COMPILER "[C++]"
