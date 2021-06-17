19208aade49d1fbccb58a0778acf967c25d29bc1
Christoph Reiter <reiter.christoph@gmail.com>
2021-06-17 18:52:14 +0530

    Use cygpty while using isatty
    so that msys shells are detected as interactive ones
    
    Co-authored-by: Алексей <alexey.pawlow@gmail.com>
    Co-authored-by: Naveen M K <naveen521kk@gmail.com>

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -1,6 +1,7 @@
 /* Built-in functions */
 
 #include "Python.h"
+#include "iscygpty.h"
 #include "pycore_ast.h"           // _PyAST_Validate()
 #include "pycore_call.h"          // _PyObject_CallNoArgs()
 #include "pycore_ceval.h"         // _PyEval_Vector()
@@ -2224,7 +2225,7 @@ builtin_input_impl(PyObject *module, PyObject *prompt)
         if (fd < 0 && PyErr_Occurred()) {
             goto error;
         }
-        tty = fd == fileno(stdin) && isatty(fd);
+        tty = fd == fileno(stdin) && (isatty(fd) || is_cygpty(fd));
     }
     if (tty) {
         tmp = PyObject_CallMethodNoArgs(fout, &_Py_ID(fileno));
@@ -2237,7 +2238,7 @@ builtin_input_impl(PyObject *module, PyObject *prompt)
             Py_DECREF(tmp);
             if (fd < 0 && PyErr_Occurred())
                 goto error;
-            tty = fd == fileno(stdout) && isatty(fd);
+            tty = fd == fileno(stdout) && (isatty(fd) || is_cygpty(fd));
         }
     }
 
