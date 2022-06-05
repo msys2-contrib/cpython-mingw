187328ad0d7a9a09bcf75714027a8d51a48ea0c6
Naveen M K <naveen521kk@gmail.com>
2022-06-05 20:28:53 +0530

    Fix failing tests
    - In `test_sysconfig`, ignore `test_sysconfig.TestSysConfig.test_user_similar` test failure.
    - Copy `get_platform()` from from distutils.utils to test_importlib/test_windows.py.
    - In `test_tcl`, ignore `test_tcl.TclTest.testLoadWithUNC` test failure.
    - Disable `test.test_asynchat.TestAsynchat.test_line_terminator2`, seems flaky.
    - skip some more flaky tests
    - some basic fixes for test_getpath
    - test_sysconfig.py: fix tests related to mingw

--- Lib/test/test_getpath.py
+++ Lib/test/test_getpath.py
@@ -838,6 +838,7 @@ DEFAULT_NAMESPACE = dict(
     ENV_PYTHONHOME="",
     ENV_PYTHONEXECUTABLE="",
     ENV___PYVENV_LAUNCHER__="",
+    ENV_MSYSTEM="",
     argv0="",
     py_setpath="",
     real_executable="",
@@ -877,6 +878,7 @@ class MockNTNamespace(dict):
         self.update(DEFAULT_NAMESPACE)
         self["config"] = DEFAULT_CONFIG.copy()
         self["os_name"] = "nt"
+        self["is_mingw"] = 0
         self["PLATLIBDIR"] = "DLLs"
         self["PYWINVER"] = "9.8-XY"
         self["VPATH"] = r"..\.."
@@ -1053,6 +1055,7 @@ class MockPosixNamespace(dict):
         self.update(DEFAULT_NAMESPACE)
         self["config"] = DEFAULT_CONFIG.copy()
         self["os_name"] = "posix"
+        self["is_mingw"] = 0
         self["PLATLIBDIR"] = "lib"
         self["WITH_NEXT_FRAMEWORK"] = 0
         super().__init__(*a, **kw)
