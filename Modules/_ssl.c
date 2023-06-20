eb0c1b78ce72ae322c8fab5755d255e5c84791f2
Naveen M K <naveen521kk@gmail.com>
2023-06-20 20:35:27 +0530

    ssl module: add MINGW support

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -5963,7 +5963,7 @@ _ssl_nid2obj_impl(PyObject *module, int nid)
     return result;
 }
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 
 static PyObject*
 certEncodingType(DWORD encodingType)
@@ -6282,7 +6282,7 @@ _ssl_enum_crls_impl(PyObject *module, const char *store_name)
     }
 }
 
-#endif /* _MSC_VER */
+#endif /* _WIN32 */
 
 /* List of functions exported by this module. */
 static PyMethodDef PySSL_methods[] = {
