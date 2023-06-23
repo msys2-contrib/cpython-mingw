6cc41eb6d690dde3c32910f8696fa1cbaa041090
Naveen M K <naveen521kk@gmail.com>
2023-06-23 20:49:15 +0530

    venv creation fixes

--- Lib/venv/__init__.py
+++ Lib/venv/__init__.py
@@ -13,6 +13,7 @@ import sysconfig
 import types
 import shlex
 
+from sysconfig import _POSIX_BUILD
 
 CORE_VENV_DEPS = ('pip',)
 logger = logging.getLogger(__name__)
@@ -330,7 +331,7 @@ class EnvBuilder:
                     if not os.path.islink(path):
                         os.chmod(path, 0o755)
         else:
-            if self.symlinks:
+            if self.symlinks and not _POSIX_BUILD:
                 # For symlinking, we need a complete copy of the root directory
                 # If symlinks fail, you'll get unnecessary copies of files, but
                 # we assume that if you've opted into symlinks on Windows then
@@ -353,6 +354,12 @@ class EnvBuilder:
                 src = os.path.join(dirname, suffix)
                 if os.path.lexists(src):
                     copier(src, os.path.join(binpath, suffix))
+
+            if _POSIX_BUILD:
+                # copy from python/pythonw so the venvlauncher magic in symlink_or_copy triggers
+                copier(os.path.join(dirname, 'python.exe'), os.path.join(binpath, 'python3.exe'))
+                copier(os.path.join(dirname, 'python.exe'), os.path.join(binpath, 'python%d.%d.exe' % sys.version_info[:2]))
+                copier(os.path.join(dirname, 'pythonw.exe'), os.path.join(binpath, 'python3w.exe'))
 
             if sysconfig.is_python_build():
                 # copy init.tcl
@@ -378,6 +385,7 @@ class EnvBuilder:
         env['VIRTUAL_ENV'] = context.env_dir
         env.pop('PYTHONHOME', None)
         env.pop('PYTHONPATH', None)
+        env.pop("MSYSTEM", None)
         kwargs['cwd'] = context.env_dir
         kwargs['executable'] = context.env_exec_cmd
         subprocess.check_output(args, **kwargs)
