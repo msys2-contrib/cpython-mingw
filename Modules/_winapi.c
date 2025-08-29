6494af6a1c99b7c8e4d7d650bf1019e147163ca2
Alexey Pavlov <alexpux@gmail.com>
2025-08-29 08:50:31 +0300

    configure: define _DEBUG for a debug build
    The Windows code expects it in some places for a debug build
    
    Skip some things which fail to link with _DEBUG

--- Modules/_winapi.c
+++ Modules/_winapi.c
@@ -1497,7 +1497,7 @@ static PyObject *
 _winapi_ExitProcess_impl(PyObject *module, UINT ExitCode)
 /*[clinic end generated code: output=a387deb651175301 input=4f05466a9406c558]*/
 {
-    #if defined(Py_DEBUG)
+    #if defined(Py_DEBUG) && defined(_MSC_VER)
 #ifdef MS_WINDOWS_DESKTOP
         SetErrorMode(SEM_FAILCRITICALERRORS|SEM_NOALIGNMENTFAULTEXCEPT|
                      SEM_NOGPFAULTERRORBOX|SEM_NOOPENFILEERRORBOX);
